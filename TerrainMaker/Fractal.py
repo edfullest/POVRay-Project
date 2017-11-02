e877339ff9f40713a322e7e99e9aa12bd828fd64
Eduardo Serna <lalotahoma@icloud.com>
2017-11-01 18:13:59 -0600

    Added main to python Mandelbrot

--- TerrainMaker/Fractal.py
+++ TerrainMaker/Fractal.py
@@ -1,4 +1,6 @@
 import numpy as np
+from pylab import *
+
 
 def mandel(a, max_iteration):
     b = a
@@ -12,3 +14,7 @@ def set(min_x,max_x,min_y,max_y,w,h,max):
     a = np.linspace(min_x, max_x, w)
     b = np.linspace(min_y, max_y, h)
     return (a,b,[mandel(complex(c, i),max) for c in a for i in b])
+
+if __name__ == "__main__":
+    M = set(0,10,0,10,10,5,10)
+    print(M)
